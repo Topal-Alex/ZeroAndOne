4baee7cded27dd1d41fd3ce79eb8f1161eeddab9
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 11:58:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            ZeroAndOne("0101010100101010101001010101001010");
+
+        }
+        static public int ZeroAndOne(string s){
+            return s.Replace("01","").Replace("10","").Length;
+
+        }
+
+    }
+
+}
